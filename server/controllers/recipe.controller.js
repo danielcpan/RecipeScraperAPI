5a9de8c45db61079eaf72a49e74785add93cb7a5
Daniel Pan <danielcpan@gmail.com>
2019-09-04 01:09:52 -0700

    minor edits

--- server/controllers/recipe.controller.js
+++ server/controllers/recipe.controller.js
@@ -1,16 +1,8 @@
 const httpStatus = require('http-status');
-// const cheerio = require("cheerio");
-// const axios = require("axios");
 
-
-// const {
-//   decimalToBaseN, withHttp, withoutWWW, getNextSequence,
-// } = require('../utils/mongoose.utils');
 const Recipe = require('../models/recipe.model');
-// const Visit = require('../models/visit.model');
 const APIError = require('../utils/APIError.utils');
 const { scrapeRecipe, scrapeCookbook } = require('../utils/recipe.utils');
-// const { PUBLIC_URL, RECIPE_URL } = require('../config/config');
 
 module.exports = {
   get: async (req, res, next) => {
@@ -31,14 +23,7 @@ module.exports = {
   },
   list: async (req, res, next) => {
     try {
-      // console.log("got here")
-      // console.log("req.params.mainIngredient: " + req.params.mainIngredient)
-      // console.log("req.params.cuisine: " + req.params.cuisine)
-      // console.log("req.params.season: " + req.params.season)
-      // const recipes = await scrapeCookbook('Shellfish/all/Fall');
-      // const recipes = {}
       const recipes = await Recipe.find({});
-
 
       return res.json(recipes)
     } catch (err) {
